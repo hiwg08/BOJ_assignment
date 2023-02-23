05ed5d25435cd254878d86052c97b2c908511776
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-02-23 17:44:30 +0900

    [Gold IV] Title: 괄호, Time: 16 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MAX 5000
+#define MOD 1000000007
+
+using namespace std;
+
+ll T, N, DP[MAX + 1]{ 0 };
+
+ll solve(ll Idx)
+{
+	if (Idx == 0)
+		return 1;
+
+	ll& ret = DP[Idx];
+
+	if (ret != -1)
+		return ret;
+	
+	ret = 0;
+
+	for (ll j = Idx - 2; j >= 0; j -= 2)
+		ret = ((ret % MOD) + (((solve(Idx - j - 2) % MOD) * (solve(j) % MOD)) % MOD)) % MOD;
+
+	return ret;
+}
+
+int main()
+{
+	fastio;
+	
+	cin >> T;
+
+	memset(DP, -1, sizeof(DP));
+
+	while (T--)
+	{
+		cin >> N;
+		N % 2 == 0 ? cout << solve(N) << '\n' : cout << 0 << '\n';
+	}
+}
