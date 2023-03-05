12ab2be19f83402317c830d71c9c4c3485ea9291
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-03-05 21:22:28 +0900

    [unrated] Title: 롤케이크 자르기, Time: 6.66 ms, Memory: 40.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> V;
+
+int sister[10001]{ 0 }, brother[10001]{ 0 };
+
+int sister_cnt, brother_cnt;
+
+int solution(vector<int> topping) { // bruteforcing + implementation
+    int answer = 0;
+    
+    V = topping;
+    
+    for (int i = (int)V.size() - 1; i >= 0; i--)
+    {
+        if (brother[V[i]] == 0)
+            brother_cnt++;
+        brother[V[i]]++;
+    }
+    
+    for (int i = 1; i < (int)V.size(); i++)
+    {
+        if (sister[V[i - 1]] == 0)
+            sister_cnt++;
+        
+        sister[V[i - 1]]++;
+        
+        if (brother[V[i - 1]] == 1)
+            brother_cnt--;
+        brother[V[i - 1]]--;
+        
+        if (sister_cnt == brother_cnt)
+            answer++;
+    }
+    
+    return answer;
+}
