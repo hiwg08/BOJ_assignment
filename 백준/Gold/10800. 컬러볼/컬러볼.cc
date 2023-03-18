52c1ddbee3aa6b3da565a8b1b0b68341b3b40f19
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-03-18 21:45:58 +0900

    [Gold III] Title: 컬러볼, Time: 156 ms, Memory: 25108 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.cc
+++ 백준/Gold/10800. 컬러볼/컬러볼.cc
@@ -1,71 +1,71 @@
 #include <bits/stdc++.h>
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define pii pair<int, int>
+#define ll long long
+#define pll pair<ll, ll>
 #define x first
 #define y second
+#define pb push_back
 
 using namespace std;
 
-int N, Sum_Huge[200001]{ 0 }, Sum_Color[200001]{ 0 }, Ans[200001]{ 0 };
+vector<pair<pll, ll>> V;
 
-vector<pair<pii, int>> Huge_V, Color_V;
+ll N;
 
-vector<int> U[200001];
+vector<pll> color_coll[200001];
 
-bool H_cmp(pair<pii, int> a, pair<pii, int> b)
+vector<pll> S;
+
+ll ans[200001]{ 0 };
+
+void Init()
 {
-	if (a.x.y == b.x.y)
-		return a.x.x < b.x.x;
-	return a.x.y < b.x.y;
+	for (ll i = 1; i <= 200000; i++)
+		color_coll[i].pb({ -1, 0 });
+
+	S.pb({ -1, 0 });
 }
-bool C_cmp(pair<pii, int> a, pair<pii, int> b)
+
+bool cmp(pair<pll, ll> a, pair<pll, ll> b)
 {
 	if (a.x.x == b.x.x)
 		return a.x.y < b.x.y;
 	return a.x.x < b.x.x;
 }
 
-void Input()
-{
-	cin >> N;
-
-	for (int i = 0, a, b; i < N; i++)
-	{
-		cin >> a >> b;
-		Huge_V.push_back({ {a, b}, i });
-		Color_V.push_back({ {a, b}, i });
-	}
-
-	sort(Huge_V.begin(), Huge_V.end(), H_cmp);
-	sort(Color_V.begin(), Color_V.end(), C_cmp);
-
-	int Chk = 0;
-
-	for (int i = 1; i <= N; i++)
-	{
-		Sum_Huge[i] = Sum_Huge[i - 1] + Huge_V[i - 1].x.y;
-
-		Sum_Color[i] = Sum_Color[i - 1] + Color_V[i - 1].x.y;
-
-		U[Color_V[i - 1].x.x].push_back(Color_V[i - 1].x.y);
-	}
-}
-
 int main()
 {
 	fastio;
 
-	Input();
+	cin >> N; V.resize(N);
 
 	for (int i = 0; i < N; i++)
 	{
-		int L = (int)(lower_bound(Huge_V.begin(), Huge_V.end(), pair<pii, int>({ {0, Huge_V[i].x.y}, 0}), H_cmp) - Huge_V.begin());
-		int Mid = (int)(lower_bound(Color_V.begin(), Color_V.end(), pair<pii, int>({ {Huge_V[i].x.x, 0}, 0 })) - Color_V.begin());
-		int R = (int)(lower_bound(U[Huge_V[i].x.x].begin(), U[Huge_V[i].x.x].end(), Huge_V[i].x.y) - U[Huge_V[i].x.x].begin()) + Mid;
-
-		Ans[Huge_V[i].y] = Sum_Huge[L] - (Sum_Color[R] - Sum_Color[Mid]);
+		cin >> V[i].x.y >> V[i].x.x;
+		V[i].y = i;
 	}
 
-	for (int i = 0; i < N; i++)
-		cout << Ans[i] << '\n';
+	sort(V.begin(), V.end(), cmp);
+
+	Init();
+
+	for (ll i = 0; i < N; i++)
+	{
+		if (S.back().x != V[i].x.x)
+			S.pb({ V[i].x.x, S.back().y + V[i].x.x });
+		else
+			S.back().y += V[i].x.x;
+
+		if (color_coll[V[i].x.y].back().x != V[i].x.x)
+			color_coll[V[i].x.y].pb({V[i].x.x, color_coll[V[i].x.y].back().y + V[i].x.x});
+		else
+			color_coll[V[i].x.y].back().y += V[i].x.x;
+		
+		ll L = S[(ll)S.size() - 2].y, R = color_coll[V[i].x.y][(ll)color_coll[V[i].x.y].size() - 2].y;
+
+		ans[V[i].y] = L - R;
+	}
+
+	for (ll i = 0; i < N; i++)
+		cout << ans[i] << '\n';
 }
