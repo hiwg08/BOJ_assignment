749c21a19ae585cf837650fdee1964a9e6afa2af
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-03-19 21:57:05 +0900

    [Platinum V] Title: 대결, Time: 0 ms, Memory: 2868 KB -BaekjoonHub

---
+++ 백준/Platinum/1489. 대결/대결.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int DP[60][60][60]{ 0 };
+
+vector<int> A, B;
+
+int solve(int a, int b, int last)
+{
+	if (a >= (int)A.size())
+		return 0;
+
+	int& ret = DP[a][b][last];
+
+	if (ret != -1)
+		return ret;
+
+	ret = 0;
+
+	if (A[a] > B[b])
+		ret += solve(a + 1, b + 1, last) + 2;
+
+	else if (A[a] == B[b])
+	{
+		int L = solve(a + 1, b, last - 1);
+		int R = solve(a + 1, b + 1, last) + 1;
+
+		ret += max(L, R);
+	}
+	else
+		ret += solve(a + 1, b, last - 1);
+
+	return ret;
+}
+
+int main()
+{
+	cin >> N; A.resize(N); B.resize(N);
+
+	for (auto& iv : A)
+		cin >> iv;
+
+	for (auto& iv : B)
+		cin >> iv;
+
+	sort(A.begin(), A.end());
+
+	sort(B.begin(), B.end());
+
+	memset(DP, -1, sizeof(DP));
+
+	cout << solve(0, 0, N - 1) << '\n';
+}
