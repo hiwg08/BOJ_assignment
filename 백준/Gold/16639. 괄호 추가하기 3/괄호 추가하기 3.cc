9ace7d9665a34b99c30803de0d70d52f2e7c5d54
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-03-29 19:36:18 +0900

    [Gold I] Title: 괄호 추가하기 3, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16639. 괄호 추가하기 3/괄호 추가하기 3.cc
+++ 백준/Gold/16639. 괄호 추가하기 3/괄호 추가하기 3.cc
@@ -20,7 +20,7 @@ void init()
 	}
 }
 
-pii solve(int L, int R)
+pii sol(int L, int R)
 {
 	if (L == R)
 		return { (int)(S[L] - '0'), (int)(S[L] - '0') };
@@ -30,11 +30,11 @@ pii solve(int L, int R)
 	if (!(ret.x == INT_MIN && ret.y == INT_MAX))
 		return ret;
 
-	ret.x = 987654321, ret.y = -987654321; // x에는 최소, y에는 최대
+	ret.x = INT_MAX, ret.y = INT_MIN; // x에는 최소, y에는 최대
 
 	for (int i = L; i <= R - 2; i += 2)
 	{
-		pii T1 = solve(L, i), T2 = solve(i + 2, R);
+		pii T1 = sol(L, i), T2 = sol(i + 2, R);
 
 		int S1 = 0, S2 = 0, S3 = 0, S4 = 0;
 
@@ -60,8 +60,8 @@ pii solve(int L, int R)
 			S4 = T1.y * T2.y;
 		}
 
-		ret.x = min(ret.x, min({S1, S2, S3, S4}));
-		ret.y = max(ret.y, max({S1, S2, S3, S4}));
+		ret.x = min(ret.x, min({ S1, S2, S3, S4 }));
+		ret.y = max(ret.y, max({ S1, S2, S3, S4 }));
 	}
 
 	return ret;
@@ -75,9 +75,8 @@ int main()
 
 	init();
 
-	cout << solve(0, N - 1).y;
+	cout << sol(0, N - 1).y;
 }
 
-// 구간 dp
 // dp[L][R] : L ~ R 만큼의 구간 상태일 때, 나올 수 있는 최솟값(first), 최댓값(second)
 // time complexity : O(N^3)
