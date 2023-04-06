25635288e1089ca9ab20ccd201424477aa04880a
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-04-06 19:41:30 +0900

    Update 구간 나누기.cc

--- 백준/Gold/2228. 구간 나누기/구간 나누기.cc
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.cc
@@ -1,31 +1,65 @@
 #include <bits/stdc++.h>
+#define pii pair<int, int>
+#define x first
+#define y second
 
 using namespace std;
 
-long long N, M, Arr[101]{ 0 }, DP[101][101]{ 0 };
+int N, M;
 
-long long Solve(long long Idx, long long Interval)
+int Arr[101]{ 0 };
+
+pii DP[101][101];
+
+void init()
 {
+	for (int i = 0; i <= 100; i++)
+	{
+		for (int j = 0; j <= 100; j++)
+			DP[i][j] = { -1, -1 };
+	}
+}
+
+pii solve(int Idx, int interval)
+{
+	if (interval == 0) // 여기는 무조건 된 것
+		return { 0, 1 };
+
 	if (Idx >= N)
 	{
-		if (Interval != 0)
-			return INT_MIN;
-		return 0;
+		if (interval > 0)
+			return { INT_MAX, 0 };
+		return { 0, 1 };
 	}
 
-	long long& ret = DP[Idx][Interval];
+	pii& ret = DP[Idx][interval];
 
-	if (ret != 0x7f7f7f7f7f7f7f7f)
+	if (ret.y != -1)
 		return ret;
 
-	ret = Solve(Idx + 1, Interval);
+	ret.x = INT_MIN, ret.y = 0;
 
-	long long temp = 0;
+	int sum = 0;
 
-	for (long long i = Idx; i < N; i++)
+	for (int i = Idx; i < N; i++)
 	{
-		temp += Arr[i];
-		ret = max(ret, Solve(i + 2, Interval - 1) + temp);
+		sum += Arr[i];
+
+		pii T1 = solve(i + 2, interval - 1);
+
+		if (T1.y == 1) // 이 경우에만 값 갱신 허용.
+		{
+			ret.y = 1;
+			ret.x = max(ret.x, sum + T1.x);
+		}
+	}
+
+	pii T1 = solve(Idx + 1, interval);
+
+	if (T1.y == 1)
+	{
+		ret.y = 1;
+		ret.x = max(ret.x, T1.x);
 	}
 
 	return ret;
@@ -38,7 +72,7 @@ int main()
 	for (int i = 0; i < N; i++)
 		cin >> Arr[i];
 
-	memset(DP, 0x7f, sizeof(DP));
+	init();
 
-	cout << Solve(0, M) << '\n';
+	cout << solve(0, M).x;
 }
