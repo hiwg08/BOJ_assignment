d58d4935d70dfd14cd51d7e49ba2d48ebbe9f911
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-04-13 16:58:07 +0900

    Update ⚾.cc

--- 백준/Gold/17281. ⚾/⚾.cc
+++ 백준/Gold/17281. ⚾/⚾.cc
@@ -12,15 +12,15 @@ int Vis[20]{ 0 };
 
 int match()
 {
-	int cnt = 0; // 점수
+	int cnt = 0;
 
-	int k = -1; // 아웃 직전의 타자.
+	int k = -1;
 
 	for (int i = 1; i <= N; i++) // 모든 이닝마다 반복
 	{
 		int out_count = 0; // 이닝이 시작할 때 아웃 카운트는 초기화
 
-		deque<int> DQ; // n루에 있는 타자들도 마찬가지로 초기화. 덱을 쓰는 이유는 나무 제테크 문제와 동일하다고 보면 된다. (항상 오름차순 또는 내림차순으로 관리되는 덱을 생각하면 됨)
+		deque<int> DQ; // n루에 있는 타자들도 마찬가지로 초기화.덱을 쓰는 이유는 나무 제테크 문제와 동일하다고 보면 된다. (항상 오름차순 또는 내림차순으로 관리되는 덱을 생각하면 됨)
 
 		if (k == -1)
 			k = 1;
@@ -30,7 +30,7 @@ int match()
 				k = 1;
 			else
 				k++;
-		}
+		} // ex. 이전 이닝에서 4번 타자에서 아웃됐으면 5번 타자로 점프, 9번 타자였다면 1번 타자로. 
 
 		for (int j = k; j <= 9; j++)
 		{
@@ -57,17 +57,17 @@ int match()
 					DQ.pop_back();
 			}
 
-			if (out_count == 3)
+			if (out_count == 3) // 3 아웃이면 그 다음 칠 타자를 정해주고 빠져나오자.
 			{
 				k = j;
 				break;
 			}
 
 			if (j == 9)
-				j = 0;
+				j = 0; // 이거 맨 처음에 int j = k라고 되어 있어도, k는 안 바꿔도 자동으로 1로 시작되더라 ㅇㅇ. 아마 int j = k는 걍 진짜 '맨 처음'의 초기값일 거야.
 		}
 	}
-	
+
 	return cnt;
 }
 
@@ -81,7 +81,7 @@ void solve(int Idx)
 
 	if (Idx == 4) // 4번 타석은 이미 1번 타자 고정이다.
 	{
-		solve(Idx + 1);
+		solve(Idx + 1); // 그래서 넘겨준 다음에 바로 리턴.
 		return;
 	}
 
@@ -100,14 +100,14 @@ void solve(int Idx)
 int main()
 {
 	cin >> N;
-
-	for (int i = 1; i <= N; i++)
+	 
+	for (int i = 1; i <= N; i++) 
 	{
 		for (int j = 1; j <= 9; j++)
 			cin >> arr[i][j];
 	}
 
-	order[4] = 1; // 4번은 1번 타자가.
+	order[4] = 1; // 4번은 1번 타자가
 
 	solve(1); // 1(Idx)번 order부터 9번 order까지 브루트포스로 정하기
 
