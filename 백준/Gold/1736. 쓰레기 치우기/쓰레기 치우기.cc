336718f0ca447cdd4f0a81fb271a89e49d5dd2b2
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-04-19 00:22:51 +0900

    Update 쓰레기 치우기.cc

--- 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.cc
+++ 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.cc
@@ -14,7 +14,7 @@ void solve(int Idx, int standard)
 		{
 			auto it = ms[i].lower_bound(standard);
 
-			if (it == ms[i].end())
+			if (it == ms[i].end()) // ms[i]가 텅 비었거나 lower_bound에 만족하는 숫자가 없을 때는 그 곳은 애당초 로봇이 쓰레기를 치울 수가 없다. 따라서 바로 빠져나오자.
 				break;
 
 			standard = *it;
@@ -34,18 +34,19 @@ void input()
 		{
 			cin >> a;
 			if (a == 1)
-				ms[i].insert(-j);
+				ms[i].insert(-j); // 배열은 굳이 필요하지 않다. multiset에 '음수'로 각 행에 있는 쓰레기의 열 값을 저장해주자. **** 가장 가까운 '왼쪽' 값을 찾기 위해서이다. ****
 		}
 	}
 }
 
-int main()
+int main() // 그리디 문제. 1. 맨 아래에서부터 보면서 왼쪽 --> 위 쪽으로 쓰레기를 치워주자.
+           // 오른쪽 -> 아래로 밖에 로봇이 움직이지 못 한다는 점, dp로 접근하기에는 점화식을 세우기 까다로웠던 점, 너무 직관적인(?) 아이디어로 인해 그리디로 생각.  
 {
 	input();
 
-	for (int i = N; i >= 1; i--)
+	for (int i = N; i >= 1; i--) // 가장 아래 행부터 탐색을 진행하자.
 	{
-		if (!ms[i].empty())
+		if (!ms[i].empty()) // 하나라도 있으면 그 곳을 치워야 한다.
 		{
 			total++;
 
@@ -55,8 +56,8 @@ int main()
 
 			ms[i].erase(it);
 
-			solve(i, T);
-		}	
+			solve(i, T); // 현재 행 & 열에서 로봇이 치울 수 있는 쓰레기를 치워주자.
+		}
 	}
 
 	cout << total;
