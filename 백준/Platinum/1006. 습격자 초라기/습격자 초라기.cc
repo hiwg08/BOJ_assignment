b0a6da980007439a6dc4b014b111c74ba90cc623
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-04-29 22:06:29 +0900

    [Platinum III] Title: 습격자 초라기, Time: 92 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Platinum/1006. 습격자 초라기/습격자 초라기.cc
+++ 백준/Platinum/1006. 습격자 초라기/습격자 초라기.cc
@@ -10,7 +10,7 @@ ll Arr_U[10001]{ 0 }, Arr_D[10001]{ 0 };
 
 ll DP[10001][4]{ 0 };
 
-ll store_u_l, store_u_r, store_d_l, store_d_r;
+ll store_u_l, store_d_l;
 
 ll solve(ll state)
 {
@@ -38,7 +38,7 @@ ll solve(ll state)
 					DP[i][1] = 1;
 			}
 		}
-		else if (i < N - 1)
+		else if (i < N - 1 || (i == N - 1 && state == 3))
 		{
 			DP[i][0] = min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] }) + 2;
 
@@ -54,39 +54,23 @@ ll solve(ll state)
 			if (Arr_D[i] + Arr_D[i - 1] <= W)
 				DP[i][3] = min(DP[i - 1][0], DP[i - 1][2]) + 1;
 		}
-		else
+		else if (i == N - 1)
 		{
-			if (state == 3) // 하나도 비우지 않았을 때
-			{
-				DP[i][0] = min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] }) + 2;
-
-				if (Arr_U[i] + Arr_U[i - 1] <= W && Arr_D[i] + Arr_D[i - 1] <= W)
-					DP[i][1] = DP[i - 1][0];
-
-				if (Arr_U[i] + Arr_D[i] <= W)
-					DP[i][1] = min(DP[i][1], min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] }) + 1);
-
-				if (Arr_U[i] + Arr_U[i - 1] <= W)
-					DP[i][2] = min(DP[i - 1][0], DP[i - 1][3]) + 1;
-
-				if (Arr_D[i] + Arr_D[i - 1] <= W)
-					DP[i][3] = min(DP[i - 1][0], DP[i - 1][2]) + 1;
-			}
-			if (state == 0) // 위 지울 때 0, 3만 있으면 됨
+			if (state == 0)
 			{
 				DP[i][0] = min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] }) + 1;
 
 				if (Arr_D[i] + Arr_D[i - 1] <= W)
 					DP[i][3] = min(DP[i - 1][0], DP[i - 1][2]);
 			}
-			if (state == 1) // 아래 지울 때 0, 2만 있으면 됨
+			if (state == 1)
 			{
 				DP[i][0] = min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] }) + 1;
 
 				if (Arr_U[i] + Arr_U[i - 1] <= W)
 					DP[i][2] = min(DP[i - 1][0], DP[i - 1][3]);
 			}
-			if (state == 2) // 전부 비웠을 때
+			if (state == 2)
 				return min({ DP[i - 1][0], DP[i - 1][1], DP[i - 1][2], DP[i - 1][3] });
 		}
 	}
@@ -112,30 +96,28 @@ int main()
 		for (ll i = 0; i < N; i++)
 			cin >> Arr_D[i];
 
-		store_u_l = Arr_U[0], store_u_r = Arr_U[N - 1], store_d_l = Arr_D[0], store_d_r = Arr_D[N - 1];
+		store_u_l = Arr_U[0], store_d_l = Arr_D[0];
+
+		total = min(total, solve(3));
 
 		if (Arr_U[0] + Arr_U[N - 1] <= W)
 		{
-			Arr_U[0] = 99999, Arr_U[N - 1] = 99999;
+			Arr_U[0] = 99999;
 			total = min(total, solve(0) + 1);
-			Arr_U[0] = store_u_l, Arr_U[N - 1] = store_u_r;
+			Arr_U[0] = store_u_l;
 		}
 		if (Arr_D[0] + Arr_D[N - 1] <= W)
 		{
-			Arr_D[0] = 99999, Arr_D[N - 1] = 99999;
+			Arr_D[0] = 99999;
 			total = min(total, solve(1) + 1);
-			Arr_D[0] = store_d_l, Arr_D[N - 1] = store_d_r;
+			Arr_D[0] = store_d_l;
 		}
 		if (Arr_U[0] + Arr_U[N - 1] <= W && Arr_D[0] + Arr_D[N - 1] <= W)
 		{
-			Arr_U[0] = 99999, Arr_U[N - 1] = 99999;
-			Arr_D[0] = 99999, Arr_D[N - 1] = 99999;
+			Arr_U[0] = 99999;
+			Arr_D[0] = 99999;
 			total = min(total, solve(2) + 2);
-			Arr_U[0] = store_u_l, Arr_U[N - 1] = store_u_r;
-			Arr_D[0] = store_d_l, Arr_D[N - 1] = store_d_r;
 		}
-
-		total = min(total, solve(3));
 
 		cout << total << '\n';
 	}
