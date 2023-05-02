c8194197a4fce7df4a84bbcd1801e0048813caaa
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-05-02 17:30:12 +0900

    [Platinum V] Title: 다이아몬드 광산, Time: 56 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.cc
+++ 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.cc
@@ -10,9 +10,7 @@ int pre_sum_se[753][753]{ 0 };
 
 int Arr[753][753]{ 0 };
 
-int R, C;
-
-int total = 0;
+int R, C, total = 0;
 
 void init_1()
 {
@@ -38,41 +36,32 @@ void init_2()
 	}
 }
 
-bool monge_in(int params, int left)
-{
-	if (left == 1)
-		return params >= 1 && params <= C; // 동, 서
-
-	return params >= 1 && params <= R; // 남, 북
-}
+// 1 ~ R이면....
+// 1 ~ C이면....
 
 void solve()
 {
-	for (int i = 2; i <= R - 1; i++)
+	for (int k = 380; k >= 1; k--)
 	{
-		for (int j = 2; j <= C - 1; j++)
+		for (int i = 1 + k; i <= R - k; i++)
 		{
-			// i, j가 중심이 된다.
+			for (int j = 1 + k; j <= C - k; j++)
+			{ // 아! 그냥 여기서 하면 되는구나!
+				int x = i, W = j - k, E = j + k;
 
-			for (int k = 1;; k++)
-			{
-				int N = i - k, S = i + k, W = j - k, E = j + k;
+				int NW = pre_sum_nw[x][W] - pre_sum_nw[x - k - 1][W + k + 1];
 
-				if (monge_in(E, 1) && monge_in(W, 1) && monge_in(S, 0) && monge_in(N, 0))
+				int NE = pre_sum_ne[x][E] - pre_sum_ne[x - k - 1][E - k - 1];
+
+				int SW = pre_sum_sw[x][W] - pre_sum_sw[x + k + 1][W + k + 1];
+
+				int SE = pre_sum_se[x][E] - pre_sum_se[x + k + 1][E - k - 1];
+
+				if (NW == k + 1 && (NW == NE && NE == SW && SW == SE && NW == SE))
 				{
-					int NW = pre_sum_nw[i][W] - pre_sum_nw[i - k - 1][W + k + 1];
-
-					int NE = pre_sum_ne[i][E] - pre_sum_ne[i - k - 1][E - k - 1];
-
-					int SW = pre_sum_sw[i][W] - pre_sum_sw[i + k + 1][W + k + 1];
-
-					int SE = pre_sum_se[i][E] - pre_sum_se[i + k + 1][E - k - 1];
-
-					if (NW == k + 1 && (NW == NE && NE == SW && SW == SE && NW == SE))
-						total = max(total, k + 1);
+					cout << k + 1;
+					exit(0);
 				}
-				else
-					break;
 			}
 		}
 	}
@@ -93,7 +82,6 @@ void input()
 			if (Arr[i][j] == 1)
 				total = 1;
 		}
-			
 	}
 }
 
