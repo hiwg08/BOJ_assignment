c8ed7b8fc2c8911caddaffa5fae30bbbab6e541c
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-07-04 19:07:10 +0900

    [Platinum II] Title: 단어 합치기, Time: 16 ms, Memory: 4540 KB -BaekjoonHub

--- 백준/Platinum/1372. 단어 합치기/단어 합치기.cc
+++ 백준/Platinum/1372. 단어 합치기/단어 합치기.cc
@@ -1,120 +1,22 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define o (ll)1
 #define pll pair<ll, ll>
+#define pls pair<ll, string>
 #define x first
 #define y second
-#define o (ll)1
 
 using namespace std;
 
 ll N;
 
-ll mini = LLONG_MAX;
+ll DP[13][1 << 13]{ 0 };
 
-vector<string> S;
+pls comp[13][13];
 
-pll DP[12][1 << 12]; // DP[last][bits] : 가장 최근에 쓰인 문자열이 last 인덱스 쪽의 문자열이며, 현재까지 쓰인 문자열의 개수(bits)일 떄, 만들 수 있는 문자열의 길이의 최솟값으로 정의
-                     // optimal substructure를 만족한다. (증명 완료)
+pll trace[13][1 << 13];
 
-pair<ll, string> comp(string bef, string pre)
-{
-	string results = "";
-
-	ll maxi = 0, chk = 0;
-
-	for (ll i = (ll)bef.length() - 1; i >= 0; i--)
-	{
-		ll precomp = (ll)bef.length() - i, cnt = 0;
-
-		for (ll j = i, k = 0; j < (ll)bef.length(); j++, k++)
-		{
-			if (bef[j] == pre[k])
-				cnt++;
-		}
-
-		if (precomp == cnt)
-			maxi = max(maxi, cnt);
-
-		chk++;
-
-		if (chk == (ll)pre.length())
-			break;
-	}
-
-	for (ll q = maxi; q < (ll)pre.length(); q++)
-		results += pre[q];
-
-	return { maxi, results };
-}
-
-pll solve(ll last, ll bits)
-{
-	if ((ll)__builtin_popcount(bits) == N)
-		return { 0, 0 };
-
-	pll& ret = DP[last][bits];
-
-	if (ret.x != -1)
-		return ret;
-
-	ret = { LLONG_MAX, 0 };
-
-	string tmp = "";
-
-	for (ll i = 0; i < N; i++)
-	{
-		if (i == last || bits & (o << i))
-			continue;
-
-		pair<ll, string> mid = comp(S[last], S[i]); 
-
-		pll T1 = solve(i, bits | (o << i));
-
-		if (ret.x > (ll)S[i].length() - mid.x + T1.x)
-		{
-			ret.x = (ll)S[i].length() - mid.x + T1.x;
-			ret.y = i;
-			tmp = mid.y;
-		}
-		else if (ret.x == (ll)S[i].length() - mid.x + T1.x)
-		{
-			if (tmp > mid.y)
-			{
-				tmp = mid.y;
-				ret.y = i;
-			}
-		}
-	}
-
-	return ret;
-}
-
-void reconstruct(ll pre, ll bits, ll last)
-{
-	if (last == -1)
-		cout << S[pre];
-
-	if ((ll)__builtin_popcount(bits) == N)
-		return;
-
-	pll T1 = solve(pre, bits);
-
-	pair<ll, string> roop = comp(S[pre], S[T1.y]);
-
-	for (ll i = roop.x; i < (ll)S[T1.y].length(); i++)
-		cout << S[T1.y][i];
-
-	reconstruct(T1.y, bits | (o << T1.y), pre);
-}
-
-void init()
-{
-	for (ll i = 0; i < 12; i++)
-	{
-		for (ll j = 0; j < (o << 12); j++)
-			DP[i][j] = { -1, -1 };
-	}
-}
+vector<string> V;
 
 bool cmp(string a, string b)
 {
@@ -125,15 +27,15 @@ void pruning()
 {
 	vector<string> tmp;
 
-	sort(S.begin(), S.end(), cmp);
+	sort(V.begin(), V.end(), cmp);
 
-	for (ll i = 0; i < (ll)S.size(); i++)
+	for (ll i = 0; i < (ll)V.size(); i++)
 	{
 		bool chk = true;
 
-		for (ll j = i + 1; j < (ll)S.size(); j++)
+		for (ll j = i + 1; j < (ll)V.size(); j++)
 		{
-			if (S[j].find(S[i]) != string::npos)
+			if (V[j].find(V[i]) != string::npos)
 			{
 				chk = false;
 				break;
@@ -141,50 +43,165 @@ void pruning()
 		}
 
 		if (chk)
-			tmp.push_back(S[i]);
+			tmp.push_back(V[i]);
 	}
 
-	S = tmp;
+	V = tmp;
 
-	N = (ll)S.size();
+	N = (ll)V.size();
 }
 
-int main()
+pls get_length(string bef, string pos)
 {
-	cin >> N; S.resize(N);
+	ll ret_l = 0;
+	string ret_s = "";
 
-	for (ll i = 0; i < N; i++)
-		cin >> S[i];
-
-	pruning();
-
-	string tmp = "";
-
-	ll Idx = 0;
-
-	for (ll i = 0; i < N; i++)
+	for (ll outer = (ll)bef.size() - 1; outer >= 0; outer--)
 	{
-		init();
+		ll cnt = 0;
 
-		pll T = solve(i, o << i);
+		bool chk = true;
 
-		if (mini > T.x + (ll)S[i].length())
+		for (ll last_pos = outer, curr = 0; (last_pos < (ll)bef.size() && curr < (ll)pos.size()); last_pos++, curr++)
 		{
-			mini = T.x + (ll)S[i].length();
-			tmp = S[i];
-			Idx = i;
-		}
-		else if (mini == T.x + (ll)S[i].length())
-		{
-			if (tmp > S[i])
+			if (bef[last_pos] == pos[curr])
+				cnt++;
+			else
 			{
-				tmp = S[i];
-				Idx = i;
+				chk = false;
+				break;
+			}
+		}
+
+		if (chk)
+			ret_l = max(ret_l, cnt);
+	}
+
+	for (ll q = ret_l; q < (ll)pos.length(); q++)
+		ret_s += pos[q];
+
+	return { ret_l, ret_s };
+}
+
+void check_mini()
+{
+	for (ll i = 0; i < (ll)V.size(); i++) // V[i] --> V[j] 순으로 문자열을 붙일 때
+	{
+		for (ll j = 0; j < (ll)V.size(); j++)
+		{
+			if (i == j)
+				continue;
+
+			comp[i][j] = get_length(V[i], V[j]);
+		}
+	}
+}
+
+ll solve(ll pos, ll bits)
+{
+	if ((ll)__builtin_popcount(bits) == N)
+		return (ll)V[pos].length();
+
+	ll& ret = DP[pos][bits];
+
+	if (ret != -1)
+		return ret;
+
+	ret = LLONG_MAX;
+
+	string c_s = "";
+
+	if (bits == 0)
+	{
+		for (ll i = 0; i < N; i++)
+		{
+			ll T1 = solve(i, bits | o << i);
+
+			if (ret > T1)
+			{
+				c_s = V[i], ret = T1;
+				trace[pos][bits] = { i, bits | o << i };
+			}
+			else if (ret == T1)
+			{
+				if (c_s > V[i])
+				{
+					c_s = V[i];
+					trace[pos][bits] = { i, bits | o << i };
+				}
 			}
 		}
 	}
 
-	init();
+	else
+	{
+		for (ll i = 0; i < N; i++) // i가 다음에 붙일 것, pos가 이전 거
+		{
+			if (i == pos || (bits & (o << i)))
+				continue;
 
-	reconstruct(Idx, o << Idx, -1);
+			pls Z1 = comp[pos][i];
+
+			ll T1 = solve(i, bits | o << i);
+
+			if (ret > T1 + (ll)V[pos].length() - Z1.x)
+			{
+				c_s = Z1.y, ret = T1 + (ll)V[pos].length() - Z1.x;
+				trace[pos][bits] = { i, bits | o << i };
+			}
+
+			else if (ret == T1 + (ll)V[pos].length() - Z1.x)
+			{
+				if (c_s > Z1.y)
+				{
+					c_s = Z1.y;
+					trace[pos][bits] = { i, bits | o << i };
+				}
+			}
+		}
+	}
+
+	return ret;
+	// 그게 아닌 경우
+}
+
+void reconstruct(ll pos, ll bits)
+{
+	if (N == (ll)__builtin_popcount(bits))
+		return;
+
+	pll R = trace[pos][bits];
+
+	if (bits == 0)
+		cout << V[R.x];
+	else
+	{
+		pls T1 = comp[pos][R.x];
+
+		for (ll i = T1.x; i < (ll)V[R.x].length(); i++)
+			cout << V[R.x][i];
+	}
+
+	reconstruct(R.x, R.y);
+}
+
+int main()
+{
+	cin >> N;
+
+	for (ll i = 0; i < N; i++)
+	{
+		string S; cin >> S;
+		V.push_back(S);
+	}
+
+	pruning();
+
+	check_mini();
+
+	memset(DP, -1, sizeof(DP));
+
+	solve(0, 0);
+
+	reconstruct(0, 0);
 }
