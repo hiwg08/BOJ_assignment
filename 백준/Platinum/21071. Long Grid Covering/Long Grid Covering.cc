56c6c89150020ef2467513b161d7b5f6a42b1199
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-07-27 17:08:17 +0900

    [Platinum I] Title: Long Grid Covering, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/21071. Long Grid Covering/Long Grid Covering.cc
+++ 백준/Platinum/21071. Long Grid Covering/Long Grid Covering.cc
@@ -1,84 +1,76 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define MOD (ll)(1e9 + 7)
+#define vl vector<ll>
+#define vvl vector<vl>
 
 using namespace std;
 
-vector<vector<ll>> basis{{ 1, 2, 1, 2, 0, 2, 0, 0, 0}, 
-						 { 1, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-						 { 0, 1, 0, 0, 0, 0, 0, 0, 0 },	
-						 { 0, 1, 0, 1, 0, 0, 0, 0, 0 }, 
-						 { 0, 0, 0, 1, 0, 0, 0, 0, 0 }, 
-						 { 0, 1, 0, 0, 0, 0, 0, 1, 0 }, 
-						 { 0, 0, 0, 0, 0, 1, 0, 0, 0 }, 
-						 { 0, 0, 0, 0, 0, 0, 1, 0, 0 },
-						 { 0, 0, 0, 0, 0, 0, 0, 1, 0 }};
+ll T, a;
 
-vector<ll> rig{23, 10, 3, 5, 2, 3, 1, 1, 0};
+ll precomp[]{ 1, 1, 3, 10, 23, 62 };
 
-ll T, N;
-
-ll prec[5]{ 0, 1, 3, 10, 23 };
-
-void mul_mod(ll& ret, ll params)
-{
-	ret *= params;
-	ret %= MOD;
-}
-
-void add_mod(ll& ret, ll params)
+void add_m(ll& ret, ll params)
 {
 	ret += params;
 	ret %= MOD;
 }
 
-vector<vector<ll>> dev_con(ll pows)
+void mul_m(ll& ret, ll params)
+{
+	ret *= params;
+	ret %= MOD;
+}
+
+vvl ori{
+	{1, 2, 6, 1, 0, MOD - 1},
+	{1, 0, 0, 0, 0, 0},
+	{0, 1, 0, 0, 0, 0},
+	{0, 0, 1, 0, 0, 0},
+	{0, 0, 0, 1, 0, 0},
+	{0, 0, 0, 0, 1, 0}
+};
+
+vvl dev_con(ll pows)
 {
 	if (pows == 1)
-		return basis;
+		return ori;
+
+	vvl ret(6, vl(6, 0));
 
 	if (pows % 2 != 0)
 	{
-		vector<vector<ll>> ret(9, vector<ll>(9, 0));
+		vvl T1 = dev_con(pows - 1);
 
-		vector<vector<ll>> T1 = dev_con(pows - 1);
-
-		for (ll i = 0; i < 9; i++)
+		for (ll i = 0; i < 6; i++)
 		{
-			for (ll j = 0; j < 9; j++)	
+			for (ll j = 0; j < 6; j++)
 			{
-				ll Q1 = 0;
-
-				for (ll k = 0; k < 9; k++)
+				for (ll k = 0; k < 6; k++)
 				{
-					ll R = basis[i][k];
-					mul_mod(R, T1[k][j]);
-					add_mod(Q1, R);
+					ll inn = ori[i][k];
+					mul_m(inn, T1[k][j]);
+					add_m(ret[i][j], inn);
 				}
-				ret[i][j] = Q1;
 			}
 		}
 
 		return ret;
 	}
+	
 
-	vector<vector<ll>> ret(9, vector<ll>(9, 0));
+	vvl T1 = dev_con(pows / 2);
 
-	vector<vector<ll>> T1 = dev_con(pows / 2);
-
-	for (ll i = 0; i < 9; i++)
+	for (ll i = 0; i < 6; i++)
 	{
-		for (ll j = 0; j < 9; j++)
+		for (ll j = 0; j < 6; j++)
 		{
-			ll Q1 = 0;
-
-			for (ll k = 0; k < 9; k++)
+			for (ll k = 0; k < 6; k++)
 			{
-				ll R = T1[i][k];
-				mul_mod(R, T1[k][j]);
-				add_mod(Q1, R);
+				ll inn = T1[i][k];
+				mul_m(inn, T1[k][j]);
+				add_m(ret[i][j], inn);
 			}
-			ret[i][j] = Q1;
 		}
 	}
 
@@ -91,25 +83,24 @@ int main()
 
 	while (T--)
 	{
-		cin >> N;
+		cin >> a;
 
-		if (N <= 4)
+		if (a <= 5)
+			cout << precomp[a] << '\n';
+		else
 		{
-			cout << prec[N] << '\n';
-			continue;
+			vvl ans = dev_con(a - 5);
+
+			ll total = 0;
+
+			for (ll i = 0; i < 6; i++)
+			{
+				ll inn = ans[0][i];
+				mul_m(inn, precomp[5 - i]);
+				add_m(total, inn);
+			}
+
+			cout << total << '\n';
 		}
-		
-		vector<vector<ll>> T1 = dev_con(N - 4);
-
-		ll ans = 0;
-
-		for (ll i = 0; i < 9; i++)
-		{
-			ll R = T1[0][i];
-			mul_mod(R, rig[i]);
-			add_mod(ans, R);
-		}
-
-		cout << ans << '\n';
 	}
 }
