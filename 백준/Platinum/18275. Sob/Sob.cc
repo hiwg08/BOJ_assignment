515713d12c5e031afd6e9c8261e58066e9fb9d93
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-08-28 12:12:39 +0900

    [Platinum I] Title: Sob, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/18275. Sob/Sob.cc
+++ 백준/Platinum/18275. Sob/Sob.cc
@@ -1,94 +1,33 @@
+// refered to official editorial
+// sol : math + greedy
+// time complexity : O(N)
+
 #include <bits/stdc++.h>
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define ll long long
-#define vl vector<ll>
-#define pb push_back
-#define o (ll)1
-#define x first
-#define y second
-
 
 using namespace std;
 
 ll N, M;
 
-ll ans[500001]{ 0 };
-
-void solve(vl a, vl b, ll pos)
-{
-	if ((ll)a.size() == 1)
-	{
-		ans[a[0]] = b[0];
-		return;
-	}
-
-	vl la, lb, ra, rb;
-
-	if (((ll)a.size() & 1) == 1)
-	{
-		if ((b[0] & pos) == pos) // 홀수일 때
-		{
-			for (ll i = 1; i < (ll)a.size(); i += 2)
-			{
-				la.pb(a[i]);
-				lb.pb(b[i + 1]);
-			}
-			
-			ra.pb(a[0]);
-			rb.pb(b[0]);
-
-			for (ll i = 2; i < (ll)a.size(); i += 2)
-			{
-				ra.pb(a[i]);
-				rb.pb(b[i - 1]);
-			}
-		}
-		else
-		{
-			for (ll i = 0; i < (ll)a.size(); i += 2)
-			{
-				la.pb(a[i]);
-				lb.pb(b[i]);
-			}
-			for (ll i = 1; i < (ll)a.size(); i += 2)
-			{
-				ra.pb(a[i]);
-				rb.pb(b[i]);
-			}
-		}
-	}
-	else
-	{
-		for (ll i = 0; i < (ll)a.size(); i += 2)
-		{
-			la.pb(a[i]);
-			(b[0] & pos) == pos ? lb.pb(b[i + 1]) : lb.pb(b[i]);
-		}
-
-		for (ll i = 1; i < (ll)a.size(); i += 2)
-		{
-			ra.pb(a[i]);
-			(b[0] & pos) == pos ? rb.pb(b[i - 1]) : rb.pb(b[i]);
-		}
-	}
-
-	solve(la, lb, pos << 1);
-	solve(ra, rb, pos << 1);
-}
-
 int main()
 {
-	fastio;
+    fastio;
+    
+    cin >> N >> M;
 
-	cin >> N >> M;
+    for (N--; N >= 0;)
+    {
+        ll tmp = M;
 
-	vl a, b;
+        while ((tmp & N) != N)
+           tmp++;
 
-	for (ll i = 0; i <= N - 1; i++)
-		a.pb(i), b.pb(i + M);
-
-	solve(a, b, 1);
-
-	for (ll i = 0; i <= N - 1; i++)
-		cout << i << " " << ans[i] << '\n';
+        for (ll i = tmp; i >= M; i--)
+        {
+            cout << N << " " << i << '\n';
+            N--;
+        }
+        M = tmp + 1;
+    }
 }
