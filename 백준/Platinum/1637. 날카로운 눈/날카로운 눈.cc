f33c06de78dd74ed9387fd29a5885ab2022005d0
hiwg08 <91325459+hiwg08@users.noreply.github.com>
2023-09-11 13:53:49 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 32 ms, Memory: 2484 KB -BaekjoonHub

---
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll N;
+
+vector<tuple<ll, ll, ll>> V;
+
+bool check(ll standard)
+{
+	ll total = 0;
+
+	for (ll i = 0; i < N; i++)
+	{
+		auto& [ni, nj, nk] = V[i];
+
+		ll Q = min(standard, nj);
+
+		if (ni <= Q && Q <= nj)
+			total += ((Q - ni) / nk) + 1;
+	}
+
+	return total & 1;
+}
+
+ll solve()
+{
+	ll lo = 0, hi = (ll)(1) << 32;
+
+	while (lo + 1 < hi)
+	{
+		ll mid = (lo + hi) >> 1;
+
+		check(mid) ? hi = mid : lo = mid;
+	}
+
+	return hi;
+}
+
+int main()
+{
+	cin >> N; V.resize(N);
+
+	ll tot = 0;
+
+	for (ll i = 0, a, b, c; i < N; i++)
+	{
+		cin >> a >> b >> c;
+		auto& [ni, nj, nk] = V[i];
+		ni = a, nj = b, nk = c;
+
+		tot += ((nj - ni) / nk) + 1;
+	}
+
+	if (!(tot & 1))
+	{
+		cout << "NOTHING";
+		exit(0);
+	}
+
+	ll ans = solve();
+
+	ll cnt = 0;
+
+	for (ll i = 0; i < N; i++)
+	{
+		auto& [ni, nj, nk] = V[i];
+
+		if (ni <= ans && ans <= nj)
+		{
+			if ((ans - ni) % nk == 0)
+				cnt++;
+		}
+	}
+
+	cout << ans << " " << cnt;
+}
