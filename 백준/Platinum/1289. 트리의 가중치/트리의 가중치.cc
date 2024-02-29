bcd55109b8377ae5b157bd03f3beddf3c07e1e64
JaeHyuk Shim <91325459+hiwg08@users.noreply.github.com>
2024-02-29 14:45:27 +0900

    [Platinum III] Title: 트리의 가중치, Time: 124 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Platinum/1289. 트리의 가중치/트리의 가중치.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pll pair<ll, ll>
+#define MOD (ll)(1e9 + 7)
+#define x first
+#define y second
+
+using namespace std;
+
+ll N;
+
+vector<pll> Tree[100001];
+
+pll dp[100001];
+
+void init()
+{
+	for (ll i = 1; i <= N; i++)
+		dp[i] = { -1, -1 };
+}
+
+void add_mod(ll& ret, ll params)
+{
+	ret += params;
+	ret %= MOD;
+}
+
+void mul_mod(ll& ret, ll params)
+{
+	ret *= params;
+	ret %= MOD;
+}
+
+pll solve(ll Idx, ll bef)
+{
+	pll& ret = dp[Idx];
+
+	if (ret.x != -1)
+		return ret;
+
+	ret = { 0, 0 };
+
+	ll sum = 0;
+
+	bool flag = false;
+
+	for (auto& e : Tree[Idx])
+	{
+		if (e.x == bef)
+			continue;
+
+		flag = true;
+
+		pll T1 = solve(e.x, Idx);
+
+		ll stat = T1.y;
+
+		add_mod(stat, 1);
+		mul_mod(stat, e.y);
+		
+		add_mod(ret.x, T1.x);
+		add_mod(ret.x, stat);
+
+		add_mod(ret.y, stat);
+
+		ll K = sum;
+		mul_mod(K, stat);
+		add_mod(ret.x, K);
+
+		add_mod(sum, stat);
+	}
+	
+	return ret;
+}
+
+int main()
+{
+	cin >> N;
+
+	for (ll i = 0, A, B, W; i < N - 1; i++)
+	{
+		cin >> A >> B >> W;
+
+		Tree[A].push_back({ B, W });
+		Tree[B].push_back({ A, W });
+	}
+
+	init();
+
+	cout << solve(1, -1).x;
+}
